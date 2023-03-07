c340cd7a7916c1bc8170ce91de2ffb88b62f9139
jolieesk <91705180+jolieesk@users.noreply.github.com>
2023-03-07 13:23:21 -0500

    Milestone #4
    getMonthText(dateArray)
    like(index)
    dislike(index)
    posterClick(index)

--- site.js
+++ site.js
@@ -24,27 +24,76 @@
 const vue_app = Vue.createApp({
       // This automatically imports your movies.json file and puts it into
       //   the variable: movies
-      created () {
+      created() {
             fetch('movies.json').then(response => response.json()).then(json => {
                   this.movies = json
             })
       },
       data() {
-        return {
-            // This holds your movies.json data.
-            movies: [],
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-            
-      const_title : "IMDB + Jolie’s Top 8 Movies",
-      const_owner : "Jolie", 
-      const_github : "http://www.github.com/jolieesk/NJIT-3",
-         
-      }
-    },
+            return {
+                  // This holds your movies.json data.
+                  movies: [],
+                  /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+                  title: " Jolie's Top 8 Movies",
+                  owner: "Jolie Eskander's",
+            }
+      },
       methods: {
-            /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+            getMonthText(dateArray) {
+                  let month = ""
+                  switch (dateArray[1]) {
+
+                        case 1:
+                              month = "January"
+                              break
+                        case 2:
+                              month = "February"
+                              break
+                        case 3:
+                              month = "March"
+                              break
+                        case 4:
+                              month = "April"
+                              break
+                        case 5:
+                              month = "May"
+                              break
+                        case 6:
+                              month = "June"
+                              break
+                        case 7:
+                              month = "July"
+                              break
+                        case 8:
+                              month = "August"
+                              break
+                        case 9:
+                              month = "September"
+                              break
+                        case 10:
+                              month = "October"
+                              break
+                        case 11:
+                              month = "November"
+                              break
+                        case 12:
+                              month = "December"
+                  }
+                  return month + " " + dateArray[2] + ", " + dateArray[0]
+            },
+            posterClick(movie) {
+                  movie.posterindex++
+                  if (movie.posterindex > movie.posters.length - 1) {
+                        movie.posterindex = 0
+                  }
+            },
+            timeText(min) {
+                  let hours = Math.floor(min / 60)
+                  let minutes = min % 60
+                  return hours + 'h ' + minutes + 'm '
+            }
+
+
       }
 })
-
 vue_app.mount("#vue_app")
-
